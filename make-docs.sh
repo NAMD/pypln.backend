deb1d791cb06bb56b2d340b6e53192b7f1abbdcf
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-04-18 17:23:07 -0300

    ENH: make-docs.sh with gh-pages

--- make-docs.sh
+++ make-docs.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+TMP_DIR=/tmp/pypln-docs
+
 # Temporary directory to store static files
-rm -rf tmp-docs
-mkdir tmp-docs
-touch tmp-docs/.nojekyll
+rm -rf $TMP_DIR
+mkdir $TMP_DIR
+touch $TMP_DIR/.nojekyll
 
 # Make Sphinx documentation
 cd doc
 make html
 cd ..
-mv doc/_build/html/* tmp-docs/
+mv doc/_build/html/* $TMP_DIR/
 rm -rf doc/_build
 
 # Make Reference documentation using epydoc
-epydoc -v -u https://github.com/namd/pypln --graph=all --parse-only --html --no-frames -o tmp-docs/reference pypln/
+epydoc -v -u https://github.com/namd/pypln --graph=all --parse-only --html \
+       --no-frames -o $TMP_DIR/reference pypln/
+
+echo 'Now execute:'
+echo '  git checkout gh-pages'
+echo "  mv $TMP_DIR/* ."
+echo "  rm -rf $TMP_DIR"
+echo "...and them git add/rm files, commit and push"
