36e1294a547b648b713257389ee5aa3f8789cf87
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-05-05 08:39:05 -0300

    started the webmonitor script

---
+++ pypln/apps/cluster/monitor.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+Web app to monitor PyPLN cluster
+
+license: GPL v3 or later
+__date__ = 5 / 13 / 12
+"""
+
+__docformat__ = "restructuredtext en"
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == "__main__":
+    app.run()
