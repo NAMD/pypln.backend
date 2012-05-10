9acd891b54c068aff958c429089cd743faea13e8
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-05-10 17:58:30 -0300

    halfway through implementint grid of plots for cluster load

--- pypln/Monitors/webmonitor/monitor.py
+++ pypln/Monitors/webmonitor/monitor.py
@@ -14,6 +14,7 @@ from flask import Flask,request, jsonify,json, make_response, render_template, f
 from pymongo import Connection
 import datetime
 import time
+from collections import defaultdict
 
 
 
@@ -25,10 +26,12 @@ Db = Connection()[app.config['DATABASE']]
 
 @app.route("/")
 def dashboard():
+    info = Db.Stats.find_one(fields=['cluster'])
+    number_nodes = len(info['cluster'])
     with open("/tmp/pypln.log") as f:
         logs = f.readlines()
     logs.reverse() # Latest entries first
-    return render_template('index.html',logs=logs)
+    return render_template('index.html',logs=logs,n_nodes = number_nodes,nrows=number_nodes/2,nnames=info['cluster'].keys())
 
 @app.route("/_get_stats")
 def get_cluster_stats():
@@ -36,13 +39,27 @@ def get_cluster_stats():
     Return status data about the cluster, such as list of nodes, network status, overall load, etc.
     :return: JSON object with the data fetched from Mongodb
     """
-    stats = Db.Stats.find(fields=['cluster','active_jobs']).sort("time_stamp",-1).limit(100)
+    stats = Db.Stats.find(fields=['cluster','active_jobs','time_stamp']).sort("time_stamp",-1).limit(100)
     e = []
+    timeseries = defaultdict(lambda:{}) #using a dictionary here to eliminate messages from the same second
+    ts = defaultdict(lambda:[])
     for d in stats:
+        for k,v in d['cluster'].iteritems():
+            timeseries[k][int(d['time_stamp'])*1000]=[v['last_report']['status']['cpu'],v['last_report']['status']['memory']]
+    for k,v in timeseries.iteritems():
+        ts[k].append({'data':zip([(i,v[i][0]) for i in sorted(v.keys())]),
+                      'label':"Percent CPU",
+                      'color':"blue"
+                      })
+        ts[k].append({'data':zip([(i,v[i][1]) for i in sorted(v.keys())]),
+                      'label':"Percent Memory",
+                      'color':"red"
+        })
+
         d.pop('_id')
 #        d.pop('time_stamp')
         e.append(d)
-    return jsonify(entries= e)
+    return json.dumps(ts)#jsonify(entries= e)
 
 @app.route("/_get_logs")
 def get_logs():
