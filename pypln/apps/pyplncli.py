7b5c9a73ac78857da3c08e80048af71148d856d0
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-05-25 08:52:14 -0300

    added subcommand frequency

--- pypln/apps/pyplncli.py
+++ pypln/apps/pyplncli.py
@@ -10,7 +10,9 @@ __docformat__ = 'restructuredtext en'
 
 
 import argparse
+# importing existing apps
 import extractor
+import frequency_counter
 
 def directory(d):
     """Check if path is a directory"""
@@ -34,6 +36,15 @@ def main():
     parser_extractor.add_argument('-g', '--gfs', action='store_true',help="Scan griGridFS under db. ignores path")
     parser_extractor.add_argument('path', metavar='p', type=directory, help="Path of directory to scan for documents")
     parser_extractor.set_defaults(func=extractor.main)
+
+    # Frequency counter parser
+    parser_freq = subparsers.add_parser("frequency",description='Perform word frequency analysis on a database collection')
+    parser_freq.add_argument('--db', '-d', help="Database")
+    parser_freq.add_argument('--col', '-c', help="Collection")
+    parser_freq.add_argument('--host', '-H', help="Mongodb Host")
+    parser_freq.add_argument('--port', '-p', help="Host port")
+    parser_freq.add_argument('--field', '-f', help="field with the text to be analysed")
+    parser_freq.set_defaults(func=frequency_counter.main)
 
 
     # Call the subcommand
