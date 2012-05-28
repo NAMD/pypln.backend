fb174a6c823f12ee25cd4ca376a0acfd1377ed7a
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-05-28 11:01:54 -0300

    improved package documentation

--- pypln/stores/__init__.py
+++ pypln/stores/__init__.py
@@ -1,4 +1,5 @@
 """
 This package contains code defining persistent stores which can be used by the sinks to  save  data to, and by 
-the servers to fetch data for further processing
+the servers to fetch data for further processing.
+The stores will serve  the purpose of ensuring the appropriate storage and retrieval of complex data structures on Mongod
 """
