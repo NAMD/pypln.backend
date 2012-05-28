36e2394111a402c614202d02a251239f9eb08864
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-05-28 19:59:35 -0300

    More tests

--- pypln/stores/ontology_store.py
+++ pypln/stores/ontology_store.py
@@ -9,7 +9,7 @@ license: GPL V3 or Later
 
 __docformat__ = 'restructuredtext en'
 
-
+import pymongo
 import rdflib
 import rdfextras
 rdfextras.registerplugins()
@@ -19,8 +19,12 @@ class Ontology(object):
     """
     Class to store and load ontologies from MOngodb
     """
-    def __init__(self):
+    def __init__(self,name,db,collection, host="127.0.0.1", port=27017):
+        self.name = name
         self.graph = None
+        self.connection = pymongo.Connection(host=host,port=port)
+        self.collection = self.connection[db][collection]
+
     def import_rdfxml(self,uri):
         """
         Reads RDF/XML from uri and
@@ -29,8 +33,24 @@ class Ontology(object):
         """
         self.graph = rdflib.Graph()
         self.graph.parse(uri,format="application/rdf+xml")
-    def _store(self):
-        pass
+    def store(self):
+        """
+        Stores ontology in Mongodb. Currently in n3 format
+        :return:
+        """
+        data = self._serialize_to_n3()
+        self.collection.insert({'name':self.name,'n3':data})
+
+    def load(self,name):
+        """
+        Loads Ontology from database and sets self.graph to it
+        :param name: Name of the ontology
+        :return: None
+        """
+        data = self.collection.find_one({'name':name})
+        self.graph = rdflib.Graph()
+        self.graph.parse(data,format = "n3")
+
     def _serialize_to_n3(self):
         """
         Returns Graph serialized to JSON
