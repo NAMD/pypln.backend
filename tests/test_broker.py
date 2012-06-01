dfc397480d187d29e78eda639ec0fb12a6636b59
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-06-01 16:12:03 -0300

    Add test_broker boilerplate

---
+++ tests/test_broker.py
@@ -0,0 +1,31 @@
+# coding: utf-8
+
+import unittest
+import os
+import signal
+from time import sleep
+from multiprocessing import Process
+import zmq
+from pypln.broker import ManagerBroker
+
+
+sleep_time = 0.15
+sleep_time_process = 0.1
+
+def run_broker(api_host_port, broadcast_host_port):
+    broker = ManagerBroker()
+    broker.connect(api_host_port, broadcast_host_port)
+    broker.run()
+
+class TestManagerBroker(unittest.TestCase):
+    def setUp(self):
+        args = (('*', 5555), ('*', 5556))
+        self.broker = Process(target=run_broker, args=args)
+        self.broker.start()
+        sleep(sleep_time_process)
+        self.context = zmq.Context()
+
+    def tearDown(self):
+        self.context.destroy()
+        os.kill(self.broker.pid, signal.SIGINT)
+        self.broker.join()
