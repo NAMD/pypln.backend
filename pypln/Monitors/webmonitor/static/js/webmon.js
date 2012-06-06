89f5e758f6c8a26ee477fb099e2a68905cd2f817
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-06-06 08:56:26 -0300

    redefined script_root variable

--- pypln/Monitors/webmonitor/static/js/webmon.js
+++ pypln/Monitors/webmonitor/static/js/webmon.js
@@ -19,50 +19,56 @@ webmon.monitor.prototype.get_ts_data = function (nnodes,nnames)
                     yaxis:{ min:0, max:100 },
                     xaxis:{ show:true, mode:"time", timeformat:"%y/%m/%d-%H:%M:%S"}
                 };
-                $.getJSON('/_get_stats', {}, function (data) {
+                $.getJSON($SCRIPT_ROOT+'/_get_stats', {}, function (data) {
                     for (var i = 0; i < nnodes ; i++) {
                         $.plot($("#placeholder" + i), data[nnames[i]], options)
                     }
                 });
+                return "passed"
             }
 
 webmon.monitor.prototype.update_logs = function () {
-
-                $.getJSON('/_get_logs', {}, function (logs) {
-                    $('#loglist').html(' ')
+                var Logs;
+                $.getJSON($SCRIPT_ROOT+'/_get_logs', {}, function (logs) {
+                    Logs = logs
+                    $('#loglist').html(' ');
                     for (var i = 0; i < logs.length; i++) {
                         var l = logs[i];
                         var logbot = '</div>';
                         if (l.level.indexOf("INFO") >= 0) {
                             var logtop = '<div class="alert alert-info"><a class="close" data-dismiss="alert" href="#">×</a>';
-                            $('#loglist').append(logtop + '<h4 class="alert-info">' + l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot)
+                            $('#loglist').append(logtop + '<h4 class="alert-info">' + l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot);
                         }
 
                         else if (l.level.indexOf('WARNING') >= 0) {
                             var logtop = '<div class="alert alert-warning"><a class="close" data-dismiss="alert" href="#">×</a>';
-                            $('#loglist').append(logtop + '<h4 class="alert-warning">' + l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot)
+                            $('#loglist').append(logtop + '<h4 class="alert-warning">' + l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot);
                         }
                         else if (l.level.indexOf('ERROR') >= 0) {
                             var logtop = '<div class="alert alert-error"><a class="close" data-dismiss="alert" href="#">×</a>';
-                            $('#loglist').append(logtop + '<h4 class="alert-error">' + l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot)
+                            $('#loglist').append(logtop + '<h4 class="alert-error">' + l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot);
                         }
 
                         else if (l.level.indexOf('DEBUG') >= 0) {
                             var logtop = '<div class="alert alert-success"><a class="close" data-dismiss="alert" href="#">×</a>';
-                            $('#loglist').append(logtop + '<h4 class="alert-success">'+ l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot)
+                            $('#loglist').append(logtop + '<h4 class="alert-success">'+ l.loggerName + '-' + l.level + '-' + l.timest + '</h4>' + l.message + logbot);
                         }
 
                     }
                 });
 
+    return Logs;
             }
 
 webmon.monitor.prototype.update_jobs = function (){
-                $.getJSON('/_get_active_jobs', {}, function (jobs) {
+                var Jobs;
+                $.getJSON($SCRIPT_ROOT+'/_get_active_jobs', {}, function (jobs) {
+                    Jobs = jobs;
                     for (var i = 0; i < jobs.length; i++) {
-                        $('#joblist').append('<li>' + jobs[i].toString() + '</li>')
+                        $('#joblist').append('<li>' + jobs[i].toString() + '</li>');
                     }
-                })
+                });
+                return Jobs;
             }
 
 
