6bdde1d8fe289fe750f104bfab0234c61f72ed51
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-06-12 22:17:08 -0300

    Add docstrings + sample on pypln.utils.monitoring

--- pypln/utils/monitoring.py
+++ pypln/utils/monitoring.py
@@ -7,13 +7,18 @@ import psutil
 
 
 def get_outgoing_ip((host, port)):
+    """Connect to remote host/port, return local IP used by OS"""
     raw_socket = socket.socket(socket.AF_INET)
     raw_socket.connect((host, port))
     data = raw_socket.getsockname()
     raw_socket.close()
-    return data
+    return data[0]
 
 def get_host_info():
+    """Return a ``dict`` with system's information
+
+    `Example of its output <https://gist.github.com/gists/2891134>`_
+    """
     memory_usage = psutil.phymem_usage()
     cached_memory = psutil.cached_phymem()
     buffered_memory = psutil.phymem_buffers()
@@ -60,6 +65,7 @@ def get_host_info():
             'uptime': time() - psutil.BOOT_TIME,}
 
 def get_process_info(process_id):
+    """Return CPU and memory information for a given PID"""
     try:
         process = psutil.Process(process_id)
     except psutil.error.NoSuchProcess:
@@ -70,3 +76,25 @@ def get_process_info(process_id):
             'virtual memory': memory_info.vms,
             'pid': process.pid,
             'started at': process.create_time,}
+
+
+if __name__ == '__main__':
+    from pprint import pprint
+    from time import time
+    from os import getpid
+
+    host_info = get_host_info()
+    host_info['network']['cluster ip'] = get_outgoing_ip(('localhost', 80))
+    broker_info = get_process_info(getpid())
+    broker_info['type'] = 'broker'
+    broker_info['active workers'] = 4
+    processes = [broker_info]
+    for job in range(broker_info['active workers']):
+        worker_info = get_process_info(getpid())
+        if worker_info is not None:
+            worker_info['worker'] = 'the worker is a lie'
+            worker_info['document id'] = '...'
+            worker_info['type'] = 'worker'
+            processes.append(worker_info)
+    data = {'host': host_info, 'processes': processes, 'timestamp': time()}
+    pprint(data)
