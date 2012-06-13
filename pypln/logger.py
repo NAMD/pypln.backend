fbdaed7072e68e288c36d535be50d64c5ab7340e
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-06-13 14:35:41 -0300

    updated

--- pypln/logger.py
+++ pypln/logger.py
@@ -16,25 +16,21 @@ from log4mongo.handlers import MongoHandler
 # Setting up the logger
 
 
-def make_log(name):
+def make_log(name,fs=False):
     """
-    Sets up the logger for eah module
-    :param name: Norammyl passed the name of the module so that we know where tha message comes from.
+    Sets up the logger for each module
+    :param name: pass the name of the module so that we know where tha message comes from.
     :return: Logger object
     """
     log = logging.getLogger(name)
     log.setLevel(logging.DEBUG)
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     # Add the log message handler to the logger
-    handler = logging.handlers.RotatingFileHandler('/tmp/pypln.log', maxBytes=200000, backupCount=1)
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
+    rfhandler = logging.handlers.RotatingFileHandler('/tmp/pypln.log', maxBytes=200000, backupCount=1)
+    rfhandler.setFormatter(formatter)
+#    log.addHandler(rfhandler)
+    log.addHandler(MongoHandler(host='localhost',database_name='pypln', formatter=formatter))
     #TODO: Maybe use zmq.loghandler
     return log
 
-def make_mongolog(name):
-    log = logging.getLogger(name)
-    log.setLevel(logging.DEBUG)
-    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-    log.addHandler(MongoHandler(host='localhost',database_name='PYPLN'))
-    return log
+
