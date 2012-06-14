d4bec9b60e8d82a05e476ace790063f486dd9e3a
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-06-14 08:56:45 -0300

    added test for get_cluster_stats

--- webmonitor/test_monitor.py
+++ webmonitor/test_monitor.py
@@ -8,6 +8,7 @@ license: GPL V3 or Later
 import unittest
 import monitor
 from pymongo import Connection
+import json
 
 monitor.Db = Connection()['pypln']
 
@@ -16,3 +17,5 @@ __docformat__ = 'restructuredtext en'
 class TestMonitor(unittest.TestCase):
     def test_fetching_xminutes_from_db(self):
         self.assertGreater(len(monitor.fetch_x_minutes(10)),0)
+    def test_getting_stats(self):
+        self.assertIsInstance(json.loads(monitor.get_cluster_stats()),dict)
