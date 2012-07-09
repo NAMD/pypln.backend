a84c51b6b79b585976fe5b1b9a693c5376caa125
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-07-09 09:43:21 -0300

    Added form validation

--- pyplnweb/pyplnweb/apps/taw/forms.py
+++ pyplnweb/pyplnweb/apps/taw/forms.py
@@ -8,8 +8,18 @@ __author__ = 'fccoelho'
 from django import forms
 from taw.models import Corpus
 
-class CorpusForm(forms.ModelForm):
-    class Meta:
-        model = Corpus
-        fields = ('title','description')
-  
+class CorpusForm(forms.Form):
+    name = forms.CharField(max_length=64,min_length=3, required=True)
+    description = forms.CharField(required=True, widget=forms.Textarea)
+    private = forms.BooleanField(required=True, initial=False)
+
+    def check_name(self):
+        """
+        Check name availability in the document store
+        :return:
+        """
+        data = self.cleaned_data['name']
+        #TODO: stubify and look it up in the Documento Store
+        if data in []:
+            raise forms.ValidationError("This name is taken. please try a new one")
+
