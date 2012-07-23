0c6c036650dc80dc74848de08fd4be2382a82c3a
Flávio Codeço Coelho <fccoelho@gmail.com>
2012-07-23 08:54:27 -0300

    added non-rest corpus url for post requests

--- pyplnweb/pyplnweb/apps/taw/urls.py
+++ pyplnweb/pyplnweb/apps/taw/urls.py
@@ -15,10 +15,11 @@ from taw.models import Document, Corpus, Glossario
 urlpatterns = patterns('taw.views', #this is the prefix to the views below:(eg. taw.views.main)
     url(r'^$', "main", name="taw_home"),
     url(r'^corpora/$', 'corpora_page', name="corpora"),
-    url(r'^corpus/(?P<document_id>\w+)/$','corpus', name="corpus"),
+    url(r'^corpus/(?P<corpus_slug>\w+)/$','corpus', name="corpus"), # restful corpus access/creation
+#    url(r'^corpus/$','corpus', name="corpus"), #for POSTs
     url(r'^collections/$', 'collection_browse', name="collections"),
     url(r'^collections/(?P<dbname>\w+)/(?P<collname>\w+)/$', 'document_browse'),
     url(r'^createcorpus/$', 'create_corpus'),
     url(r'^search/?', 'search', name='search'),
-    url(r'^document/(?P<document_id>.*)', 'document', name='document'),
+    url(r'^document/(?P<document_id>.*)', 'document_view', name='document'),
 )
