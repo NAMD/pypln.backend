f59c6050f759ff7242714110a94da73a00c09325
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-08-16 14:13:07 -0300

    Worker tokenizer also create 'sentences'

--- pypln/workers/tokenizer.py
+++ pypln/workers/tokenizer.py
@@ -1,14 +1,17 @@
 # coding: utf-8
 
-from nltk import word_tokenize
+from nltk import word_tokenize, sent_tokenize
 
 
 __meta__ = {'from': 'document',
             'requires': ['text'],
             'to': 'document',
-            'provides': ['tokens'],}
+            'provides': ['tokens', 'sentences'],}
 
 def main(document):
     text = document['text']
-    result = word_tokenize(text)
-    return {'tokens': result}
+    tokens = word_tokenize(text)
+    sentences = []
+    for sentence in sent_tokenize(text):
+        sentences.append(word_tokenize(sentence))
+    return {'tokens': tokens, 'sentences': sentences}
