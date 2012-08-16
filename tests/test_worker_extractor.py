6b9a90b792f7d762b5ad9605728d802668edb225
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-08-16 11:00:28 -0300

    Enhance worker extractor
    - Lots of enhancements on parse_html function to refelect a text most like the
      original HTM
    - Use pdftohtml command and then parse_html to a better text output

--- tests/test_worker_extractor.py
+++ tests/test_worker_extractor.py
@@ -1,43 +1,34 @@
 # coding: utf-8
 
 import unittest
+from textwrap import dedent
 from pypln.workers import extractor
 
 
-def clear_text(text):
-    text = text.replace('\n', '')
-    text = text.strip()
-    while '  ' in text:
-        text = text.replace('  ', ' ')
-    text = text.replace(' . ', '.')
-    return text
-
-original_text = "This is a test file.I'm testing PyPLN extractor worker!"
-
 class TestExtractorWorker(unittest.TestCase):
     def test_extraction_from_text_file(self):
+        expected = "This is a test file.\nI'm testing PyPLN extractor worker!"
         filename = 'tests/data/test.txt'
         data = {'filename': filename, 'contents': open(filename).read()}
         result = extractor.main(data)
-        text = clear_text(result['text'])
         metadata = result['metadata']
-        self.assertEqual(original_text, text)
+        self.assertEqual(expected, result['text'])
         self.assertEqual(metadata, None)
 
     def test_extraction_from_html_file(self):
+        expected = "This is a test file. I'm testing PyPLN extractor worker!"
         filename = 'tests/data/test.html'
         data = {'filename': filename, 'contents': open(filename).read()}
         result = extractor.main(data)
-        text = clear_text(result['text'])
         metadata = result['metadata']
-        self.assertEqual(original_text, text)
+        self.assertEqual(expected, result['text'])
         self.assertEqual(metadata, None)
 
     def test_extraction_from_pdf_file(self):
+        expected = "This is a test file. I'm testing PyPLN extractor worker!"
         filename = 'tests/data/test.pdf'
         data = {'filename': filename, 'contents': open(filename).read()}
         result = extractor.main(data)
-        text = clear_text(result['text'])
         metadata = result['metadata']
         metadata_expected = {
                 'Author':         'Álvaro Justen',
@@ -51,5 +42,50 @@ class TestExtractorWorker(unittest.TestCase):
                 'Optimized':      'no',
                 'PDF version':    '1.4',
         }
-        self.assertEqual(original_text, text)
+        self.assertEqual(expected, result['text'])
         self.assertEqual(metadata, metadata_expected)
+
+    def test_extraction_from_html(self):
+        contents = dedent('''
+        <html>
+          <head>
+            <title>Testing</title>
+            <script type="text/javascript">this must not appear</script>
+            <style type="text/css">this must not appear [2]</style>
+          </head>
+          <body>
+            python test1
+            <br>
+            test2
+            <table>
+              <tr><td>spam</td></tr>
+              <tr><td>eggs</td></tr>
+              <tr><td>ham</td></tr>
+            </table>
+            test3
+            <div>test4</div>test5
+            <span>test6</span>test7
+            <h1>bla1</h1> bla2
+          </body>
+        </html>
+        ''')
+        data = {'filename': 'test.html', 'contents': contents}
+        result = extractor.main(data)
+        expected = dedent('''
+            Testing
+
+            python test1
+            test2
+
+            spam
+            eggs
+            ham
+
+            test3
+            test4
+            test5 test6 test7
+
+            bla1
+
+            bla2''').strip()
+        self.assertEqual(result['text'], expected)
