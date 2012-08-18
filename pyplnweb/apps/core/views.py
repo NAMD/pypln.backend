9a1ff63055b56e3c03d9f29291f09738ff3ca0d8
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-08-17 21:23:18 -0300

    Move visualizations' "views" to its own file

--- pyplnweb/apps/core/views.py
+++ pyplnweb/apps/core/views.py
@@ -16,73 +16,9 @@ from core.util import LANGUAGES
 from pyplnweb.settings import (MANAGER_API_HOST_PORT, MANAGER_TIMEOUT,
                                MONGODB_CONFIG)
 from pypln.client import ManagerClient
+from pyplnweb.apps.core.visualizations import VISUALIZATIONS
 from mongodict import MongoDict
 
-
-def _token_frequency_histogram(data):
-    from collections import Counter
-
-    freqdist = data['freqdist']
-    values = Counter()
-    for key, value in freqdist:
-        values[value] += 1
-    data['values'] = [list(x) for x in values.most_common()]
-    del data['freqdist']
-    return data
-
-def _pos_highlighter(data):
-    tags = set([x[1] for x in data['pos']])
-    return {'tags': tags}
-
-def _statistics(data):
-    data['repertoire'] = '{:.2f}'.format(data['repertoire'] * 100)
-    data['average_sentence_repertoire'] = \
-            '{:.2f}'.format(data['average_sentence_repertoire'] * 100)
-    data['average_sentence_length'] = '{:.2f}'.format(data['average_sentence_length'])
-    data['number_of_tokens'] = len(data['tokens'])
-    data['number_of_unique_tokens'] = len(set(data['tokens']))
-    sentences = []
-    for sentence in data['sentences']:
-        sentences.append(' '.join(sentence))
-    data['number_of_sentences'] = len(sentences)
-    data['number_of_unique_sentences'] = len(set(sentences))
-    data['percentual_tokens'] = '{:.2f}'.format(100 * data['number_of_unique_tokens'] / data['number_of_tokens'])
-    data['percentual_sentences'] = '{:.2f}'.format(100 * data['number_of_unique_sentences'] / data['number_of_sentences'])
-    return data
-
-def _wordcloud(data):
-    data['freqdist'] = [[repr(x[0])[2:-1], x[1]] for x in data['freqdist']]
-    return data
-
-VISUALIZATIONS = {
-        'text': {
-            'label': _('Plain text'),
-            'requires': set(['text']),
-        },
-        'pos-highlighter': {
-            'label': _('Part-of-speech'),
-            'requires': set(['pos', 'tokens']),
-            'process': _pos_highlighter
-        },
-        'token-frequency-histogram': {
-             'label': _('Token frequency histogram'),
-             'requires': set(['freqdist', 'momentum_1', 'momentum_2',
-                              'momentum_3', 'momentum_4']),
-             'process': _token_frequency_histogram,
-        },
-        'statistics': {
-            'label': _('Statistics'),
-            'requires': set(['tokens', 'sentences', 'repertoire',
-                             'average_sentence_repertoire',
-                             'average_sentence_length']),
-            'process': _statistics,
-        },
-        'word-cloud': {
-            'label': _('Word cloud'),
-            'requires': set(['freqdist']),
-            'process': _wordcloud,
-        },
-}
 
 def _slug(filename):
     return '.'.join([slugify(x) for x in filename.split('.')])
