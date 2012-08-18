ae22c6e7d4370c2ecdc8743f2ea5a3be970346a0
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-08-17 21:07:24 -0300

    Add wordcloud visualization and enhance statistics

--- pyplnweb/apps/core/views.py
+++ pyplnweb/apps/core/views.py
@@ -30,6 +30,29 @@ def _token_frequency_histogram(data):
     del data['freqdist']
     return data
 
+def _pos_highlighter(data):
+    tags = set([x[1] for x in data['pos']])
+    return {'tags': tags}
+
+def _statistics(data):
+    data['repertoire'] = '{:.2f}'.format(data['repertoire'] * 100)
+    data['average_sentence_repertoire'] = \
+            '{:.2f}'.format(data['average_sentence_repertoire'] * 100)
+    data['average_sentence_length'] = '{:.2f}'.format(data['average_sentence_length'])
+    data['number_of_tokens'] = len(data['tokens'])
+    data['number_of_unique_tokens'] = len(set(data['tokens']))
+    sentences = []
+    for sentence in data['sentences']:
+        sentences.append(' '.join(sentence))
+    data['number_of_sentences'] = len(sentences)
+    data['number_of_unique_sentences'] = len(set(sentences))
+    data['percentual_tokens'] = '{:.2f}'.format(100 * data['number_of_unique_tokens'] / data['number_of_tokens'])
+    data['percentual_sentences'] = '{:.2f}'.format(100 * data['number_of_unique_sentences'] / data['number_of_sentences'])
+    return data
+
+def _wordcloud(data):
+    data['freqdist'] = [[repr(x[0])[2:-1], x[1]] for x in data['freqdist']]
+    return data
 
 VISUALIZATIONS = {
         'text': {
@@ -38,7 +61,8 @@ VISUALIZATIONS = {
         },
         'pos-highlighter': {
             'label': _('Part-of-speech'),
-            'requires': set(['pos', 'tokens'])
+            'requires': set(['pos', 'tokens']),
+            'process': _pos_highlighter
         },
         'token-frequency-histogram': {
              'label': _('Token frequency histogram'),
@@ -50,7 +74,13 @@ VISUALIZATIONS = {
             'label': _('Statistics'),
             'requires': set(['tokens', 'sentences', 'repertoire',
                              'average_sentence_repertoire',
-                             'average_sentence_length'])
+                             'average_sentence_length']),
+            'process': _statistics,
+        },
+        'word-cloud': {
+            'label': _('Word cloud'),
+            'requires': set(['freqdist']),
+            'process': _wordcloud,
         },
 }
 
@@ -154,7 +184,7 @@ def document_page(request, document_slug):
                       database=MONGODB_CONFIG['database'],
                       collection=MONGODB_CONFIG['analysis_collection'])
     analysis = set(store.get('id:{}:analysis'.format(document.id), []))
-    metadata = store.get('id:{}:file-metadata'.format(document.id), {})
+    metadata = store.get('id:{}:file_metadata'.format(document.id), {})
     language = store.get('id:{}:language'.format(document.id), None)
     if language is not None:
         metadata['language'] = LANGUAGES[language]
