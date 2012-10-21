f8f113f9d762c582bfa757b73130bd7a4ba36c76
Flavio Amieiro <amieiro.flavio@gmail.com>
2012-10-21 16:15:19 -0200

    Use exec in the script that runs the fastcgi server.

--- pypln/web/runfcgi.sh
+++ pypln/web/runfcgi.sh
@@ -5,4 +5,4 @@ FCGI_PORT="8080"
 ERRLOG="$HOME/logs/fcgi_server.error"
 OUTLOG="$HOME/logs/fcgi_server.out"
 
-python manage.py runfcgi method=threaded minspare=$MINSPARE maxspare=$MAXSPARE daemonize=false host=127.0.0.1 port=$FCGI_PORT errlog=$ERRLOG outlog=$OUTLOG
+exec python manage.py runfcgi method=threaded minspare=$MINSPARE maxspare=$MAXSPARE daemonize=false host=127.0.0.1 port=$FCGI_PORT errlog=$ERRLOG outlog=$OUTLOG
