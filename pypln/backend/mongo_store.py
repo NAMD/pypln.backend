02714fc86ec022d975cabca20d40e7576d1746aa
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2012-10-26 17:02:30 -0200

    mongo_store: add tests, docstrings and bug fixes

--- pypln/backend/mongo_store.py
+++ pypln/backend/mongo_store.py
@@ -7,66 +7,77 @@ from mongodict import MongoDict
 
 
 class MongoDBStore(object):
-    def __init__(self, host, port, database, analysis_collection,
-                 gridfs_collection, monitoring_collection, username=None,
-                 password=None):
+    '''Store PyPLN workers' analysis in MongoDB, using MongoDict'''
+
+    def __init__(self, **config):
+        host, port, database = config['host'], config['port'], \
+                               config['database']
         self._connection = Connection(host, port)
         self._db = self._connection[database]
-        if username is not None and password is not None:
+        if 'username' in config and 'password' in config:
            self._db.authenticate(username, password)
         self._dict = MongoDict(host=host, port=port, database=database,
-                               collection=analysis_collection)
+                               collection=config['analysis_collection'])
         #TODO: use auth on mongodict
-        self._monitoring = self._db[monitoring_collection]
-        self._gridfs = GridFS(self._db, gridfs_collection)
+        self._monitoring = self._db[config['monitoring_collection']]
+        self._gridfs = GridFS(self._db, config['gridfs_collection'])
 
-    def save(self, data):
-        worker_input = data['worker_input']
-        worker_output = data['worker_output']
-        worker_provides = data['worker_provides']
-        job_data = data['data']
-        result = data['result']
-        if worker_output == 'document':
-            if 'id' not in job_data:
-                raise ValueError('Invalid job data')
-            if worker_input == 'gridfs-file':
-                if '_id' not in job_data:
-                    raise ValueError('Invalid job data')
-            total = []
-            for key in worker_provides:
-                if key in result:
-                    new_key = 'id:{}:{}'.format(job_data['id'], key)
-                    self._dict[new_key] = result[key]
-                    total.append(key)
-            analysis_key = 'id:{}:analysis'.format(job_data['id'])
-            try:
-                self._dict[analysis_key] += total
-                #TODO: change to 'push'
-            except KeyError:
-                self._dict[analysis_key] = total
+    def retrieve(self, info):
+        '''Retrieve data to pass to `WorkerClass.process`
 
-    def save_monitoring(self, data):
-        self._monitoring.insert(data)
+        `info` has keys 'worker', 'worker_requires' and 'data':
+            - 'data' comes from pipeline data
+            - 'worker' is the worker name
+            - 'worker_requires' is 'requires' attribute of WorkerClass
+        '''
+        data = info['data']
 
-    def retrieve(self, data):
-        worker_input = data['worker_input']
-        worker_requires = data['worker_requires']
-        job_data = data['data']
-
-        result = {}
-        if worker_input == 'document':
-            if 'id' not in job_data:
-                raise ValueError('Invalid job data')
-            for key in worker_requires:
-                new_key = 'id:{}:{}'.format(job_data['id'], key)
-                result[key] = self._dict[new_key]
-        elif worker_input == 'gridfs-file':
-            if '_id' not in job_data:
-                raise ValueError('Invalid job data')
-            file_data = self._gridfs.get(ObjectId(job_data['_id']))
+        result = {'_missing': []}
+        if info['worker'] == 'Extractor':
+            if '_id' not in data:
+                raise ValueError('Invalid job data: missing "_id"')
+            file_data = self._gridfs.get(ObjectId(data['_id']))
             result = {'length': file_data.length,
                       'md5': file_data.md5,
                       'filename': file_data.filename,
                       'upload_date': file_data.upload_date,
                       'contents': file_data.read()}
+        else:
+            if 'id' not in data:
+                raise ValueError('Invalid job data: missing "id"')
+            for key in info['worker_requires']:
+                new_key = 'id:{}:{}'.format(data['id'], key)
+                try:
+                    result[key] = self._dict[new_key]
+                except KeyError:
+                    result['_missing'].append(key)
         return result
+
+    def save(self, info):
+        '''Save information returned by `WorkerClass.process`
+
+        `info` has keys 'worker', 'worker_requires', 'worker_result' and 'data':
+            - 'data' comes from pipeline data
+            - 'worker' is the worker name
+            - 'worker_requires' is 'requires' attribute of WorkerClass
+            - 'worker_result' is what WorkerClass.process returned
+        '''
+        data = info['data']
+        worker_result = info['worker_result']
+        if 'id' not in data:
+            raise ValueError('Invalid job data: missing "id"')
+
+        # insert results
+        for key, value in worker_result.items():
+            new_key = 'id:{}:{}'.format(data['id'], key)
+            self._dict[new_key] = worker_result[key]
+
+        # update property list for this document
+        properties_key = 'id:{}:_properties'.format(data['id'])
+        all_properties = self._dict.get(properties_key, []) + \
+                         worker_result.keys()
+        self._dict[properties_key] = list(set(all_properties))
+
+    def save_monitoring(self, info):
+        '''Save broker's monitoring information'''
+        self._monitoring.insert(info)
