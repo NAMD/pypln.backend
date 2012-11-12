907a98d0749cb90b9afddf8000719e24e124800c
Flavio Amieiro <amieiro.flavio@gmail.com>
2012-11-12 12:51:04 -0200

    Run fastcgi with the production settings file

--- pypln/web/runfcgi.sh
+++ pypln/web/runfcgi.sh
@@ -23,6 +23,7 @@ MAXSPARE=4
 FCGI_PORT="8080"
 ERRLOG="$PYPLN_HOME/logs/fcgi_server.error"
 OUTLOG="$PYPLN_HOME/logs/fcgi_server.out"
+SETTINGS="settings.production"
 
 source "$PYPLN_HOME/project/bin/activate"
-exec python manage.py runfcgi method=threaded minspare=$MINSPARE maxspare=$MAXSPARE daemonize=false host=127.0.0.1 port=$FCGI_PORT errlog=$ERRLOG outlog=$OUTLOG
+exec python manage.py runfcgi method=threaded minspare=$MINSPARE maxspare=$MAXSPARE daemonize=false host=127.0.0.1 port=$FCGI_PORT errlog=$ERRLOG outlog=$OUTLOG --settings=$SETTINGS
