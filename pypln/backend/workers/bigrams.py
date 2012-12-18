201d6f8eb8a3adb6aa7f23252e1ff47287becf0f
Flávio C. Coelho <fccoelho@gmail.com>
2012-12-18 19:38:47 -0200

    Added bigram worker: returns a pickled bigram finder object

---
+++ pypln/backend/workers/bigrams.py
@@ -0,0 +1,46 @@
+# coding: utf-8
+#
+# Copyright 2012 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+
+from pypelinin import Worker
+
+import nltk
+from nltk.collocations import BigramCollocationFinder
+import cPickle
+
+
+def _put_offset(text, tagged_text):
+    result = []
+    position = 0
+    for token, classification in tagged_text:
+        token_position = text.find(token, position)
+        result.append((token, classification, token_position))
+        position = token_position + len(token) - 1
+    return result
+
+class Bigrams(Worker):
+    """
+    Returns pickled bigram finder
+    """
+    requires = ['tokens']
+    bigram_measures = nltk.collocations.BigramAssocMeasures()
+
+    def process(self, tokens):
+        bigram_finder = BigramCollocationFinder.from_words(tokens)
+
+        return {'bigram_finder': cPickle.dumps(bigram_finder)}
