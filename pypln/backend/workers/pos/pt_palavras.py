2e09f240d46353a22739856733542e946e2f9226
Álvaro Justen aka Turicas <alvaro@justen.eng.br>
2013-01-23 11:38:08 -0200

    Adds Palavras part-of-speech tagger to worker POS
    Refactors POS worker (now it's a Python module instead of single file)

---
+++ pypln/backend/workers/pos/pt_palavras.py
@@ -0,0 +1,86 @@
+# coding: utf-8
+#
+# Copyright 2012 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+
+import os
+import subprocess
+import sys
+
+
+PALAVRAS_ENCODING = sys.getfilesystemencoding()
+BASE_PARSER = '/opt/palavras/por.pl'
+PARSER_MODE = '--syn'
+WORD_CLASSES = {
+                'N': 'Nouns',
+                'PROP': 'Proper nouns',
+                'SPEC': 'Specifiers',
+                'DET': 'Determiners',
+                'PERS': 'Personal pronouns',
+                'ADJ': 'Adjectives',
+                'ADV': 'Adverbs',
+                'V': 'Verbs',
+                'NUM': 'Numerals',
+                'PRP': 'Preposition',
+                'KS': 'Subordinating conjunctions',
+                'KC': 'Coordinationg conjunctions',
+                'IN': 'Interjections',
+                'EC': 'Hyphen-separated prefix',
+                'BL': 'Blank Line',
+                'ES': 'End of Sentence',
+                'NW': 'Non Word',
+}
+
+def call_palavras(text):
+    process = subprocess.Popen([BASE_PARSER, PARSER_MODE],
+                               stdin=subprocess.PIPE,
+                               stdout=subprocess.PIPE,
+                               stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate(text.encode(PALAVRAS_ENCODING))
+    return stdout
+
+def pos(text):
+    palavras_output = call_palavras(text)
+    tagged_text = []
+    for line in palavras_output.split('\n')[1:]:
+        line = line.strip().decode(PALAVRAS_ENCODING)
+        if line.isspace() or line == '':
+            continue
+        elif line.startswith('<'):
+            continue
+        elif line.startswith('$'):
+            non_word = line.split()[0][1:]
+            if non_word.isdigit():
+                non_word_tag = 'NUM'
+            else:
+                non_word_tag = non_word
+            tagged_text.append((non_word, non_word_tag))
+        elif len(line.split('\t')) < 2: # Discard malformed lines
+            continue
+        else:
+            info = line.split('\t')
+            final = '\t'.join(info[1:]).split()
+            word = info[0].strip()
+            syntatic_semantic_tags = final[1:]
+            tags = filter(lambda x: x in WORD_CLASSES, syntatic_semantic_tags)
+            if tags:
+                pos_tag = tags[0]
+                tagged_text.append((word, pos_tag))
+    return tagged_text
+
+def palavras_installed():
+    return os.path.exists(BASE_PARSER)
---
+++ tests/test_worker_pos_pt_palavras.py
@@ -0,0 +1,49 @@
+# coding: utf-8
+#
+# Copyright 2012 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+
+import unittest
+
+from textwrap import dedent
+
+from pypln.backend.workers.pos import pt_palavras
+
+
+class TestPosWorker(unittest.TestCase):
+    def test_(self):
+        palavras_output = dedent('''
+        $START
+        Eu\t[eu] <*> PERS M/F 1S NOM @SUBJ>
+        sei\t[saber] V PR 1S IND VFIN @FMV
+        que\t[que] KS @#FS-<ACC @SUB
+        em\t[em] <sam-> PRP @PIV>
+        este\t[este] <-sam> <dem> DET M S @>N
+        momento\t[momento] <dur> <f-q> N M S @P<
+        falo\t[falar] <vH> V PR 1S IND VFIN @FMV
+        para\t[para] PRP @<ADVL
+        todo\t[todo] <quant> DET M S @>N
+        Brasil\t[Brasil] <*> <newlex> PROP M S @P< \t[Brasil] <*> PROP M S @P<
+        $.
+        ''').strip()
+        expected = [('Eu', 'PERS'), ('sei', 'V'), ('que', 'KS'), ('em', 'PRP'),
+                    ('este', 'DET'), ('momento', 'N'), ('falo', 'V'),
+                    ('para', 'PRP'), ('todo', 'DET'), ('Brasil', 'PROP'),
+                    ('.', '.')]
+        pt_palavras.call_palavras = lambda x: palavras_output
+        result = pt_palavras.pos('anything')
+        self.assertEqual(expected, result)
