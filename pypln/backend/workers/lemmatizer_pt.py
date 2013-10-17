81de8a61426379a63f18df58748349945b40fa6c
Flavio C. Coelho <fccoelho@gmail.com>
2013-10-17 16:42:54 -0300

    worker for Noun-phrase extraction and lemmatization added

---
+++ pypln/backend/workers/lemmatizer_pt.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+#
+# Copyright 2012 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+
+
+import subprocess
+from pypelinin import Worker
+
+PALAVRAS_PATH = '/opt/palavras/'
+
+class Lemmatizer(Worker):
+    """Lemmatizer"""
+    requires = ['palavras_raw']
+
+    def process(self, document):
+
+        process = subprocess.Popen(PALAVRAS_PATH+'por.pl', stdin=subprocess.PIPE, stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE)
+        stdout, stderr = process.communicate(document['palavras_raw'])
+        linhas = stdout.split('\n')
+        lemmas = []
+        for l in linhas:
+            if l.startswith('$'):
+                lemmas.append(l.split('#').strip().strip('$'))
+            else:
+                lemmas.append(l.split('[')[1].split(']')[0])
+
+        return {'lemmas': lemmas}
+
+
