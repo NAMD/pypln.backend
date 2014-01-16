b2e4dd83d7eae919b9f6222c4567224a48dfabc7
Flavio Amieiro <amieiro.flavio@gmail.com>
2014-01-16 18:49:46 -0200

    Filters stopwords and punctuation before generating the wordcloud

--- pypln/backend/workers/word_cloud.py
+++ pypln/backend/workers/word_cloud.py
@@ -18,18 +18,31 @@
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
 
 import base64
+import string
 from StringIO import StringIO
 
 import numpy
+import nltk
 from wordcloud import make_wordcloud
 
 from pypelinin import Worker
 
+def filter_stopwords(fdist, lang):
+    long_name = {'en': 'english', 'pt': 'portuguese'}
+    stopwords =  list(string.punctuation)
+    if lang in long_name:
+        # nltk should give is unicode objects.
+        # https://github.com/nltk/nltk/issues/122 says this is already
+        # implemented in a branch, so for now we'll use this workaround.
+        stopwords += map(lambda w: w.decode('utf-8'),
+                nltk.corpus.stopwords.words(long_name[lang]))
+    return filter(lambda pair: pair[0].lower() not in stopwords, fdist)
+
 class WordCloud(Worker):
-    requires = ['freqdist']
+    requires = ['freqdist', 'language']
 
     def process(self, document):
-        fdist = document['freqdist']
+        fdist = filter_stopwords(document['freqdist'], document['language'])
         words = numpy.array([t[0] for t in fdist])
         counts = numpy.array([t[1] for t in fdist])
         wordcloud_img = make_wordcloud(words, counts,
