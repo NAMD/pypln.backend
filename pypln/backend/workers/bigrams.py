6aefdaa5e4518e88ac642709f26d351b47565ead
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-04-19 19:00:51 +0200

    Migrates Bigrams worker to a Celery Task
    The regression tests for pickling where deleted because now the base class
    `PyPLNTask` uses MongoDict (and consequently pickles the content) when passing
    arguments to `Bigrams.process` and getting the results, so any test that calls
    `Bigrams.delay` will test that the pickling is working.

--- pypln/backend/workers/bigrams.py
+++ pypln/backend/workers/bigrams.py
@@ -17,19 +17,15 @@
 # You should have received a copy of the GNU General Public License
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
 
-import cPickle
-
 import nltk
 from collections import defaultdict
 
 from nltk.collocations import BigramCollocationFinder
-from pypelinin import Worker
+from pypln.backend.celery_task import PyPLNTask
 
 
-class Bigrams(Worker):
+class Bigrams(PyPLNTask):
     """Create a NLTK bigram finder and return a table in JSON format"""
-    requires = ['tokens']
-
 
     def process(self, document):
         #todo: support filtering by stopwords
--- tests/test_worker_bigrams.py
+++ tests/test_worker_bigrams.py
@@ -17,59 +17,27 @@
 # You should have received a copy of the GNU General Public License
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
 
-import cPickle
-import gridfs
 import nltk
-import unittest
 
-from mongodict import MongoDict
-from pymongo import Connection
-
-from pypln.backend.mongo_store import MongoDBStore
 from pypln.backend.workers.bigrams import Bigrams
-from .utils import default_config
+from utils import TaskTest
 
 bigram_measures = nltk.collocations.BigramAssocMeasures()
 
 
-class TestBigramWorker(unittest.TestCase):
-    def _prepare_store(self):
-        self.db_conf = db_conf = default_config['store']
-        self.connection = Connection(host=db_conf['host'],
-                                     port=db_conf['port'])
-        self.connection.drop_database(db_conf['database'])
-        self.db = self.connection[db_conf['database']]
-        self.monitoring = self.db[db_conf['monitoring_collection']]
-        self.gridfs = gridfs.GridFS(self.db, db_conf['gridfs_collection'])
-        self.db[db_conf['gridfs_collection'] + '.files'].drop()
-        self.db[db_conf['gridfs_collection'] + '.chunks'].drop()
-        self.mongodict = MongoDict(host=db_conf['host'], port=db_conf['port'],
-                                   database=db_conf['database'],
-                                   collection=db_conf['analysis_collection'])
-        self.store = MongoDBStore(**db_conf)
-
+class TestBigramWorker(TaskTest):
     def test_bigrams_should_return_correct_score(self):
-        tokens = nltk.corpus.genesis.words('english-web.txt')
+        # We need this list comprehension because we need to save the word list
+        # in MongoDict (thus, it needs to be pickleable). Also, a list is what
+        # will be available to the worker in real situations.
+        tokens = [w for w in
+                nltk.corpus.genesis.words('english-web.txt')]
+
+        self.document['tokens'] = tokens
         bigram_finder = nltk.collocations.BigramCollocationFinder.from_words(tokens)
         expected = bigram_finder.score_ngram(bigram_measures.chi_sq, u',', u'which')
-        bigram_rank = Bigrams().process({'tokens':tokens})['bigram_rank']
+
+        Bigrams().delay(self.fake_id)
+        bigram_rank = self.document['bigram_rank']
         result = bigram_rank[0][1][0]
         self.assertEqual(result, expected)
-
-    def test_worker_output_should_be_pickleable(self):
-        """The workers run under multiprocessing, so their result is
-        pickled. This is a regression test."""
-        tokens = nltk.corpus.genesis.words('english-web.txt')
-        result = Bigrams().process({'tokens':tokens})
-        # This should not raise an exception.
-        cPickle.dumps(result)
-
-    def test_saving_worker_output_should_work(self):
-        """Saving the worker output should work. This is a regression test."""
-        self._prepare_store()
-        tokens = nltk.corpus.genesis.words('english-web.txt')[:100]
-        result = Bigrams().process({'tokens': tokens})
-        info = {'data': {'id': 789, '_id': 'eggs'}, 'worker': 'Bigrams',
-                'worker_requires': ['tokens'], 'worker_result': result}
-        self.store.save(info)
-        self.connection.drop_database(self.db)
