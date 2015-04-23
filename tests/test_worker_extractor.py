f6173dd62bdf876aa360c441d94a44b840f5701a
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-04-23 18:56:50 +0200

    Changes all the Extractor tests to use it as a Celery task

--- tests/test_worker_extractor.py
+++ tests/test_worker_extractor.py
@@ -39,18 +39,22 @@ class TestExtractorWorker(TaskTest):
         expected = "This is a test file. I'm testing PyPLN extractor worker!"
         filename = os.path.join(DATA_DIR, 'test.html')
         data = {'filename': filename, 'contents': open(filename).read()}
-        result = Extractor().process(data)
-        metadata = result['file_metadata']
-        self.assertEqual(expected, result['text'])
-        self.assertEqual(metadata, {})
-        self.assertEqual(result['mimetype'], 'text/html')
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['text'], expected)
+        self.assertEqual(self.document['file_metadata'], {})
+        self.assertEqual(self.document['mimetype'], 'text/html')
 
     def test_extraction_from_pdf_file(self):
         expected = "This is a test file.\nI'm testing PyPLN extractor worker!"
         filename = os.path.join(DATA_DIR, 'test.pdf')
         data = {'filename': filename, 'contents': open(filename).read()}
-        result = Extractor().process(data)
-        metadata = result['file_metadata']
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['text'], expected)
+        # Check that the expected metadata is a subset of what
+        # our Extractor found (it may have found more details
+        # depending on the toolset used to extract metadata)
         metadata_expected = {
                 'Author':         'Álvaro Justen',
                 'Creator':        'Writer',
@@ -63,18 +67,15 @@ class TestExtractorWorker(TaskTest):
                 'Optimized':      'no',
                 'PDF version':    '1.4',
         }
-        self.assertEqual(expected, result['text'])
-        # Check that the expected metadata is a subset of what
-        # our Extractor found (it may have found more details
-        # depending on the toolset used to extract metadata)
         metadata_expected_set = set(metadata_expected.iteritems())
+        metadata = self.document['file_metadata']
         metadata_set = set(metadata.iteritems())
         diff_set = metadata_expected_set - metadata_set
         self.assertTrue(metadata_expected_set.issubset(metadata_set),
                         ("Extracted metadata is not a subset of the expected metadata. "
                          "Items missing or with different values: {}").format(
                          u", ".join(unicode(item) for item in diff_set)))
-        self.assertEqual(result['mimetype'], 'application/pdf')
+        self.assertEqual(self.document['mimetype'], 'application/pdf')
 
     def test_extraction_from_html(self):
         contents = dedent('''
@@ -101,7 +102,8 @@ class TestExtractorWorker(TaskTest):
         </html>
         ''')
         data = {'filename': 'test.html', 'contents': contents}
-        result = Extractor().process(data)
+        self.document.update(data)
+        result = Extractor().delay(self.fake_id)
         expected = dedent('''
             Testing
 
@@ -119,45 +121,55 @@ class TestExtractorWorker(TaskTest):
             bla1
 
             bla2''').strip()
-        self.assertEqual(result['text'], expected)
-        self.assertEqual(result['mimetype'], 'text/html')
+        self.assertEqual(self.document['text'], expected)
+        self.assertEqual(self.document['mimetype'], 'text/html')
 
-    def test_language_detection(self):
+    def test_language_detection_pt(self):
         text_pt = 'Esse texto foi escrito por Álvaro em Português.'
-        text_es = 'Este texto ha sido escrito en Español por Álvaro.'
-        text_en = 'This text was written by Álvaro in English.'
         data_pt = {'filename': 'text-pt.txt', 'contents': text_pt}
+        self.document.update(data_pt)
+        Extractor().delay(self.fake_id).get()
+        self.assertEqual(self.document['language'], 'pt')
+
+    def test_language_detection_es(self):
+        text_es = 'Este texto ha sido escrito en Español por Álvaro.'
         data_es = {'filename': 'text-es.txt', 'contents': text_es}
+        self.document.update(data_es)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['language'], 'es')
+
+    def test_language_detection_en(self):
+        text_en = 'This text was written by Álvaro in English.'
         data_en = {'filename': 'text-en.txt', 'contents': text_en}
-        result_pt = Extractor().process(data_pt)
-        result_es = Extractor().process(data_es)
-        result_en = Extractor().process(data_en)
-        self.assertEqual('pt', result_pt['language'])
-        self.assertEqual('es', result_es['language'])
-        self.assertEqual('en', result_en['language'])
+        self.document.update(data_en)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['language'], 'en')
 
     def test_unescape_html_entities(self):
         expected = (u"This text has html <entities>. Álvaro asked me to make"
                      " sure it also has non ascii chars.")
         filename = os.path.join(DATA_DIR, 'test_html_entities.txt')
         data = {'filename': filename, 'contents': open(filename).read()}
-        result = Extractor().process(data)
-        self.assertEqual(expected, result['text'])
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['text'], expected)
 
     def test_should_detect_encoding_and_return_a_unicode_object(self):
         expected = u"Flávio"
         filename = os.path.join(DATA_DIR, 'test_iso-8859-1.txt')
         data = {'filename': filename, 'contents': open(filename).read()}
-        result = Extractor().process(data)
-        self.assertEqual(expected, result['text'])
-        self.assertEqual(type(result['text']), unicode)
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['text'], expected)
+        self.assertEqual(type(self.document['text']), unicode)
 
     def test_should_guess_mimetype_for_file_without_extension(self):
         contents = "This is a test file. I'm testing PyPLN extractor worker!"
         filename = os.path.join(DATA_DIR, 'text_file')
         data = {'filename': filename, 'contents': contents}
-        result = Extractor().process(data)
-        self.assertEqual(result['mimetype'], 'text/plain')
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['mimetype'], 'text/plain')
 
     def test_unknown_mimetype_should_be_flagged(self):
         filename = os.path.join(DATA_DIR, 'random_file')
@@ -165,18 +177,19 @@ class TestExtractorWorker(TaskTest):
         # it's equal to the input content, since
         contents = open(filename).read()
         data = {'filename': filename, 'contents': contents}
-        result = Extractor().process(data)
-        self.assertEqual(result['mimetype'], 'unknown')
-        self.assertEqual(result['text'], "")
-        self.assertEqual(result['language'], "")
-        self.assertEqual(result['file_metadata'], {})
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['mimetype'], 'unknown')
+        self.assertEqual(self.document['text'], "")
+        self.assertEqual(self.document['language'], "")
+        self.assertEqual(self.document['file_metadata'], {})
 
     def test_unknown_encoding_should_be_ignored(self):
         filename = os.path.join(DATA_DIR, 'encoding_unknown_to_libmagic.txt')
         expected = u"This file has a weird byte (\x96) that makes it impossible for libmagic to recognize it's encoding."
         data = {'filename': filename, 'contents': open(filename).read()}
-        result = Extractor().process(data)
-        metadata = result['file_metadata']
-        self.assertEqual(expected, result['text'])
-        self.assertEqual(result['file_metadata'], {})
-        self.assertEqual(result['language'], 'en')
+        self.document.update(data)
+        Extractor().delay(self.fake_id)
+        self.assertEqual(self.document['text'], expected)
+        self.assertEqual(self.document['file_metadata'], {})
+        self.assertEqual(self.document['language'], 'en')
