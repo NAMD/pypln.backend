e2f3ec3f4283fdeada56a552dd209a93959a7bb9
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-04-30 17:26:21 +0200

    Adds GridFSDataRetriever to the exported attributes of pypln.backend.workers

--- pypln/backend/workers/__init__.py
+++ pypln/backend/workers/__init__.py
@@ -31,6 +31,6 @@ from palavras_semantic_tagger import SemanticTagger
 from word_cloud import WordCloud
 
 
-__all__ = ['Extractor', 'Tokenizer', 'FreqDist', 'POS', 'Statistics',
-           'Bigrams', 'PalavrasRaw', 'Lemmatizer', 'NounPhrase',
-           'SemanticTagger', 'WordCloud']
+__all__ = ['GridFSDataRetriever', 'Extractor', 'Tokenizer', 'FreqDist', 'POS',
+            'Statistics', 'Bigrams', 'PalavrasRaw', 'Lemmatizer', 'NounPhrase',
+            'SemanticTagger', 'WordCloud']
