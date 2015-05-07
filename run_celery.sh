3d06fb32fd0c04c4c4ed9f8ef2134a38f26d79bb
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-05-07 16:26:48 +0200

    Adds script to run celery in production

---
+++ run_celery.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Copyright 2015 NAMD-EMAP-FGV
+#
+# This file is part of PyPLN. You can get more information at: http://pypln.org/.
+#
+# PyPLN is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# PyPLN is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+
+PYPLN_HOME="/srv/pypln"
+
+source "$PYPLN_HOME/project/bin/activate"
+cd "$PYPLN_HOME/project/backend/"
+exec celery -A 'pypln.backend' worker --app=pypln.backend.celery_app:app  -l info
