38e3f7e5a91f5705713d690c8d8b348f18e848c6
Flavio Amieiro <amieiro.flavio@gmail.com>
2015-08-10 18:12:46 +0200

    Changes GridFSDataRetriever to base64 encode the contents before storing
    This is necessary because the content of the file might be binary data (such as
    a pdf), and we don't want to change it by encoding/decoding. This was not an
    issue before because MongoDict used to pickle this before storing. This kind of
    adaptation is probably going to happen in other places where the mapping from
    python objects to json is not straight forward.

--- pypln/backend/workers/gridfs_data_retriever.py
+++ pypln/backend/workers/gridfs_data_retriever.py
@@ -16,6 +16,7 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with PyPLN.  If not, see <http://www.gnu.org/licenses/>.
+import base64
 from bson import ObjectId
 from gridfs import GridFS
 import pymongo
@@ -31,9 +32,17 @@ class GridFSDataRetriever(PyPLNTask):
         gridfs = GridFS(database, config.MONGODB_CONFIG['gridfs_collection'])
 
         file_data = gridfs.get(ObjectId(document['file_id']))
+
+        # We decided to store 'contents' as a base64 encoded string in the
+        # database to avoid possible corruption of files. For example: when
+        # it's a pdf, the process of storing the data as utf-8 in mongo might
+        # be corrupting the file.  This wasn't a problem before, because
+        # MongoDict pickled everything before storing.
+        contents = base64.b64encode(file_data.read())
+
         result = {'length': file_data.length,
                   'md5': file_data.md5,
                   'filename': file_data.filename,
                   'upload_date': file_data.upload_date,
-                  'contents': file_data.read()}
+                  'contents': contents}
         return result
