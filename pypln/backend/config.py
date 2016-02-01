44da6a01e43bd1e88a7ed767f45d6538ade304b4
Eduardo Stalinho <eduardooc.86@gmail.com>
2016-02-01 14:56:00 -0200

    no need of functions to get configuration

--- pypln/backend/config.py
+++ pypln/backend/config.py
@@ -26,29 +26,18 @@ def parse_url(url):
 
     return config
 
-def get_store_config():
-    store_config = config('MONGODB_CONFIG',
+MONGODB_CONFIG = config('MONGODB_CONFIG',
             default='mongodb://localhost:27017/pypln_dev', cast=parse_url)
-    collection = config('MONGO_COLLECTION', default='documents')
-    gridfs_collection = config('GRIDFS_COLLECTION', default='files')
-    store_config.update({
-        'collection': collection,
-        'gridfs_collection': gridfs_collection
-    })
-    return store_config
+MONGODB_CONFIG['collection'] =  config('MONGO_COLLECTION', default='analysis')
+MONGODB_CONFIG['gridfs_collection'] = config('GRIDFS_COLLECTION', default='files')
 
-MONGODB_CONFIG = get_store_config()
 ELASTICSEARCH_CONFIG = {
     'hosts': config('ELASTICSEARCH_HOSTS',
         default='127.0.0.1,172.16.4.46,172.16.4.52', cast=Csv())
 }
 
-def get_broker_config():
-    celery_config = config('CELERY_CONFIG',
-        default='celery://guest:guest@localhost:5672', cast=parse_url)
-    return celery_config
-
-CELERY_BROKER_CONFIG = get_broker_config()
+CELERY_BROKER_CONFIG = config('CELERY_CONFIG',
+        default='amqp://guest:guest@localhost:5672', cast=parse_url)
 
 BROKER_URL = 'amqp://{}:{}@{}:{}//'.format(
         CELERY_BROKER_CONFIG['user'], CELERY_BROKER_CONFIG['password'],
